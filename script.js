5c0d8567705efe51fdaa1e4a94c6c2fa16da623d
Swayam Shrestha <swayams24@gmail.com>
2024-08-13 15:27:08 +1000

    Add function to push new Books to the library

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, hasRead = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = hasRead;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  let title = "Sixty Shades of Coding";
+  let author = "Johhny";
+  let pages = "245";
+  let hasRead = true;
+  myLibrary.push(new Book(title, author, pages, hasRead));
 }
+
+addBookToLibrary();
+console.log(myLibrary);
